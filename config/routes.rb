273533e4f9faf54f75ef5a2e9df7c30a4e4a2e81
Ashalu <developer.aashish02@gmail.com>
2018-10-11 12:26:26 +0530

    api implemented

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   resources :welcome
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :sessions, only: [:create, :destroy]
+    end
+
+  end
 end
